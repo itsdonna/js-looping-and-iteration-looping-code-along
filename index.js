56d7390d59548970bb904ccfe722cb70bf3f19ac
Donna Torres <todonnatorres@icloud.com>
2021-02-01 00:41:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+/* Code your solutions in this file
+    4 statements make up loop -  
+    for ([initialization]; [condition]; [iteration]) {
+    [loop body]
+ } */
+
+ 
+ function writeCards(name, event) {
+    const cards = []
+    for (let i = 0; i < name.length; i++ ) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num > 0 ) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
